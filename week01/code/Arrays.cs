5b3e38c90dfbd71ddca64477bd04bb5cf664ed43
benwasden <was24001@byui.edu>
2025-07-05 08:56:18 -0700

    Week 1 arrays project

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 public static class Arrays
 {
     /// <summary>
@@ -9,11 +11,23 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        
+        // I need to create an array that only accepts doubles, then multiply the number by 1, then 2, then continue that until
+        // I've multiplied it by "length" amount of times. I need to add these numbers to the array each time too, and then
+        // return the array at the end.
 
-        return []; // replace this return statement with your own
+        // Creating the array
+        double[] results = new double[length];
+
+        // Setting up a loop that starts at i = 1 and then proceeds until i is the same length as the specified amount
+        for (int i = 1; i <= length; i++)
+        {
+            // Specifies that for the i - 1 position in the array, the value is the number times i, giving the multiple
+            results[i - 1] = (number * i);
+        }
+
+        // Returns the array
+        return results; // replace this return statement with your own
     }
 
     /// <summary>
@@ -26,8 +40,22 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+
+        // I'll create a list with the numbers that I'm removing first. Then I'll get the values that I'm removing
+        // from the data list and add those to the oldNums list. Then I'll actually remove the numbers and add them
+        // back on.
+
+        // Creating the list to add the old numbers to
+        List<int> oldNums = new();
+
+        // Getting the data that will be changed. I need to go from index 0 to the length of the array minus the specified amount
+        var dataToChange = data.GetRange(0, data.Count() - amount);
+        // Adding the aquired numbers from the prior step to the oldNums list
+        oldNums.AddRange(dataToChange);
+
+        // Removing the numbers from the data list. Again, just like the dataToChange var, I need to remove from 0 to data.Count - specified amount
+        data.RemoveRange(0, data.Count() - amount);
+        // Adding the numbers that were removed back onto the end of the data list
+        data.AddRange(oldNums);
     }
 }
